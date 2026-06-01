3f5e3bb7c86b863be6bbc5deadd4f975dbd59b43
gou-xinjie <2562755718@qq.com>
2026-06-01 18:31:58 +0800

    docs(aliyun-column): 新增并完善阿里云专栏文章

--- docs/column/Aliyun/list.ts
+++ docs/column/Aliyun/list.ts
@@ -8,7 +8,8 @@ const aliyunList = [
       { text: "阿里云 ECS 入门介绍" },
       { text: "为什么前端项目部署需要nginx" },
       { text: "GitHub Actions 核心概念详解" },
-      { text: "PM2在Next项目部署中的作用" }
+      { text: "PM2在Next项目部署中的作用" },
+      { text: 'SQLite 和 better-sqlite3介绍' }
     ]
   },
   {
@@ -18,6 +19,7 @@ const aliyunList = [
       { text: "VitePress博客自动部署到ECS" },
       { text: "Next项目自动部署到ECS-初始版" },
       { text: "Next项目自动部署到ECS-优化版" },
+      { text: 'Nuxt项目自动部署到ECS' }
 
     ]
   },
