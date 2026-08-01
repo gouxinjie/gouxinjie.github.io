7e6b9fb05661033ad9589be7f2e5e0e552bd9dbf
gou-xinjie <2562755718@qq.com>
2026-08-01 09:36:42 +0800

    refactor(Docker): 移动《为什么使用 Docker 镜像》到基础模块并更新菜单

--- docs/column/Docker/list.ts
+++ docs/column/Docker/list.ts
@@ -7,18 +7,19 @@ const dockerList = [
     items: [
       { text: "核心概念和安装" },
       { text: "Docker的镜像与容器" },
+      { text: "为什么使用 Docker 镜像" },
       { text: "Dockerfile" },
       { text: "Docker Compose" },
       { text: "Docker网络" },
       { text: "容器存储" },
       { text: "容器日志和监控" },
-      { text: "Docker 镜像与网络隔离" }
     ]
   },
   {
     text: "进阶",
     collapsed: false,
     items: [
+      { text: "Docker 镜像与网络隔离" },
       { text: "Docker本地部署CSR前端项目" },
       { text: "Docker本地部署SSR前端项目" }
     ]
