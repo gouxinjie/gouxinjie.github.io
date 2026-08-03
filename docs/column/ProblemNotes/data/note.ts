965b2f897fe2928ce34cd0ac1fcb3d8c7d3862ad
gou-xinjie <2562755718@qq.com>
2026-08-03 09:44:46 +0800

    refactor(ProblemNotes): 删除日常笔记中4篇无图片文章及菜单项

--- docs/column/ProblemNotes/data/note.ts
+++ docs/column/ProblemNotes/data/note.ts
@@ -16,19 +16,10 @@ export const noteData = [
         text: "JavaScript常用函数与算法精粹"
       },
       {
-        text: "页面阅读位置记忆功能实现方案"
-      },
-      {
-        text: "前端本地开发构建和更新的过程"
-      },
-      {
         text: 'SVG与Canvas水印添加技术对比'
       },
       {
         text: 'APP扫码登录的实现原理'
-      },
-      {
-        text: '共享单车扫码开锁的技术剖析'
       },
       {
         text: 'CDN资源不会产生跨域问题'
@@ -40,13 +31,13 @@ export const noteData = [
         text: '顶部阅读进度条的方案实现'
       },
       {
-        text: '不依赖点击的MouseEvent事件触发机制'
-      },
-      {
         text: '移动端H5适配方案实践'
       },
       {
         text: '邀请新用户返利链接的实现原理'
+      },
+      {
+        text: '现代桌面应用跳转浏览器登录方案解析'
       }
     ]
   }
