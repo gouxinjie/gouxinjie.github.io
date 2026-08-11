d95173a596ab4bc4d021ec83e4449503bec11e7f
gouxinjie <gouxinjie@users.noreply.github.com>
2026-08-11 01:41:42 +0000

    deploy: 4d2c46782e1989ece617ad16e52e9f99b73a8c26

--- assets/chunks/index.Cq3dVpsc.js
+++
@@ -1,9 +0,0 @@
-const i=`
-<script setup>
-import { ref,shallowRef } from 'vue'
-import { transformMiniProgramList } from "./list";
-const MiniList = shallowRef(transformMiniProgramList("/column/miniProgram/", true));
-<\/script>
-
-<SearchList title="小程序相关" :data="MiniList" ></SearchList>
-`;export{i as default};
