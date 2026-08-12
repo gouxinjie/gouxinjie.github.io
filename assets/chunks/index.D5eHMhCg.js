6a27bfae4f8317de05da0eabe235dbcd5e9fab4c
gouxinjie <gouxinjie@users.noreply.github.com>
2026-08-12 05:57:16 +0000

    deploy: fe73a3bc9e92d0c44813236306d9af1a8fc8aa44

---
+++ assets/chunks/index.D5eHMhCg.js
@@ -0,0 +1,14 @@
+const n=`---
+layout: page
+title: Xinjie
+titleTemplate: "blog"
+editLink: false
+lastUpdated: false
+---
+
+<XinjieHome />
+
+<script setup>
+import { NAV_DATA } from './utils/homenav-data.ts'
+<\/script>
+`;export{n as default};
