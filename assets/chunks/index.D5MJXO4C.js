1780d848916510d301d443e8f785333f8124517a
gouxinjie <gouxinjie@users.noreply.github.com>
2026-08-12 06:11:34 +0000

    deploy: 4360272ba977a2e157925b7869d019917b8006e7

---
+++ assets/chunks/index.D5MJXO4C.js
@@ -0,0 +1,15 @@
+const n=`---
+layout: home
+title: Xinjie
+titleTemplate: "blog"
+editLink: false
+lastUpdated: false
+
+hero:
+  name: ""
+  text: ""
+  tagline: ""
+
+features: []
+---
+`;export{n as default};
